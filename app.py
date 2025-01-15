2d8e0b58ab538ab6d65f3d90eab1c3429b237086
thanhnew2001 <thanh.hispvietnam@gmail.com>
2025-01-16 07:29:38 +1100

    update path to file

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def home():
-    return render_template('record.html')  # Make sure the HTML file name is correct
+    return render_template('record.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000, debug=True)
